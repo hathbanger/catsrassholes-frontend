d3c3657f0f574a26f0d75b0764d20528e724db89
hathbanger <hathbanger.ah@gmail.com>
2016-11-14 09:00:45 -0700

    cleanup

--- components/Feed.js
+++ components/Feed.js
@@ -6,16 +6,12 @@ import catPics from '../catPics'
 export default class Feed extends Component {
   render() {
     const { dispatch, errorMessage, posts } = this.props
-    let woo = this.props
     console.log('this.props.posts', this.props.posts)
-    const self = this
     return (
         <ul className="list-group">
-        {this.props.posts ? &&
           {catPics.map(function(pic, index){
             return <FeedItem dispatch={dispatch} key={ index } feedItem={pic} />
           })}
-        }
         </ul>
     )
   }
